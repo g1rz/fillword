88d253047e66ac26215e1bf66ed9277e34708924
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-11-26 17:56:26 +0800

    начал создание массива

--- app/js/createWordsArr.js
+++ app/js/createWordsArr.js
@@ -4,14 +4,39 @@ const words = [
     'заявка',
     'приват',
     'документ',
-    'терминал2'
+    'терминал'
 ];
 
 
 const countChars = words.reduce((sum, item) => sum += item.length, 0);
 
-console.log(countChars, getSizeArray(countChars));
+const sizeArr = getSizeArray(countChars);
 
+let gameArr = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x).fill(0) );
+
+// console.log(countChars, sizeArr);
+console.log(gameArr);
+
+let wordObj = {
+    chars: Array.from('этикетка').reverse(),
+    x: 0,
+    y: 0,
+    direction: 'right'
+};
+
+wordObj.x = Math.floor(Math.random() * (sizeArr.x + 1));
+wordObj.y = Math.floor(Math.random() * (sizeArr.y + 1));
+
+console.log(wordObj);
+
+while (wordObj.chars.length > 0) {
+    const char = wordObj.chars.pop();
+    
+    gameArr[wordObj.y][wordObj.x] = char;
+
+    console.log(gameArr);
+    break
+}
 
 
 function getSizeArray(countItems) {
