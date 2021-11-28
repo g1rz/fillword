538b5ec2ec770599280a6874d97c758497dd1e2b
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-11-28 15:19:05 +0800

    Создание поля на JS и рандомное вставка слова в поле

--- app/js/createWordsArr.js
+++ app/js/createWordsArr.js
@@ -21,22 +21,50 @@ let wordObj = {
     chars: Array.from('этикетка').reverse(),
     x: 0,
     y: 0,
-    direction: 'right'
+    direction: ''
 };
 
-wordObj.x = Math.floor(Math.random() * (sizeArr.x + 1));
-wordObj.y = Math.floor(Math.random() * (sizeArr.y + 1));
+
 
 console.log(wordObj);
 
+let i = 1;
 while (wordObj.chars.length > 0) {
     const char = wordObj.chars.pop();
+
+    if (wordObj.direction === '') {
+        wordObj.x = getRandomInt(0, sizeArr.x - 1); 
+        wordObj.y = getRandomInt(0, sizeArr.y - 1);
+
+        
+    } else {
+        switch(wordObj.direction) {
+            case 'top':
+                wordObj.y = wordObj.y - 1;
+                break;
+            case 'right':
+                wordObj.x = wordObj.x + 1;
+                break;
+            case 'bottom':
+                wordObj.y = wordObj.y + 1;
+                break;
+            case 'left':
+                wordObj.x = wordObj.x - 1;
+        }
+        
+    }
+
+    wordObj.direction = findDirection(wordObj.x, wordObj.y, gameArr);
     
     gameArr[wordObj.y][wordObj.x] = char;
 
-    console.log(gameArr);
-    break
+    // console.log('-------');
+    // console.log('stage ', i++);
+    // console.log(wordObj);
+    // console.log(gameArr);
 }
+
+drawField(gameArr);
 
 
 function getSizeArray(countItems) {
@@ -55,4 +83,88 @@ function getSizeArray(countItems) {
     }
 
     return size;
+}
+
+function findDirection(x, y, game) {
+    let arrDirs = [];
+
+    if ( (y - 1 >= 0) && !game[y - 1][x] ) {
+        arrDirs.push('top');
+    }
+    if ( (x + 1 <= game[0].length - 1) && !game[y][x + 1] ) {
+        arrDirs.push('right');
+    }
+    if ( (y + 1 <= game.length - 1) && !game[y + 1][x] ) {
+        arrDirs.push('bottom');
+    }
+    if ( (x - 1 >= 0) && !game[y][x - 1] ) {
+        arrDirs.push('left')
+    }
+
+    return arrDirs[0];
+}
+
+
+function  getRandomInt(min, max) {
+    return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = getRandomInt(0, i);
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function drawField(gameArr) {
+    console.log('draw');
+    const $field = document.getElementById('game');
+
+    for ( let y = 0; i < gameArr.length; y++ ) {
+        const $fieldRow = document.createElement('div');
+        $fieldRow.classList.add('field__row');
+
+        for ( let x = 0; x < gameArr[y].length; x++ ) {
+
+            const $fieldItem = createElement('div', 'field__item'),
+                  $fieldItemSpan =  createElement('span', '', gameArr[y][x]),
+                  $borders = createElement('div', 'borders'),
+                  $border_top = createElement('div', 'borders__border borders__border--top'),
+                  $border_right = createElement('div', 'borders__border borders__border--right'),
+                  $border_bottom = createElement('div', 'borders__border borders__border--bottom'),
+                  $border_left = createElement('div', 'borders__border borders__border--left');
+
+            if ( !gameArr[y][x] ) {
+                $fieldItem.classList.add('field__item--empty');
+            }
+
+            $fieldItem.appendChild($fieldItemSpan);
+
+            $borders.appendChild($border_top);
+            $borders.appendChild($border_right);
+            $borders.appendChild($border_bottom);
+            $borders.appendChild($border_left);
+
+            $fieldItem.appendChild($borders);
+
+            $fieldRow.appendChild($fieldItem);
+        }
+        console.log($fieldRow);
+        $field.appendChild($fieldRow);
+    }
+}
+
+
+// classNames - строка классов, классы разделены пробелами
+function createElement(tag, classNames = '', innerText = '') {
+    const $element = document.createElement(tag);
+
+    if (classNames) 
+        $element.classList.add(...classNames.split(' '));
+    
+    if (innerText)
+        $element.innerText = innerText;
+
+    return $element;
 }
