399f5ed1f71f0b47e77a98b4f695f48cda260f3b
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-12-02 01:16:12 +0800

    Добавил поле для ввода

--- app/js/createWordsArr.js
+++ app/js/createWordsArr.js
@@ -1,254 +1,53 @@
-const words = [
-    'этикетка',
-    'город',
-    'заявка',
-    'приват',
-    'документ',
-    'терминал',
-    'эпидемия',
-    'морковка',
-    'привет',
-    'монопенисуально',
-    'рефрежератор',
-    'обороноспособность',
-    'стройка',
-    'диалог',
-    'монумент',
-    'кирпич',
-    'олимпиада'
-];
+let words = [];
 
-let colors = [
+const colors = [
     '#28f141', '#285cf1', '#d328f1', '#f12897', '#cfc868', '#68cfc6', '#fd501c', '#fdd71c', '#1cfdde', '#1caafd', '#fd25d8', '#5adfff', '#ff5a71', '#ffcc5a', '#6cff5a', '#395cda', '#8d39da', '#e9024c', '#90e902', '#e9d702'
 ];
 
-let = colorIndex = 0;
+const $btn = document.getElementById('create');
 
-colors = shuffle(colors);
+const $hideColor = document.getElementById('hide_color');
 
-const countChars = words.reduce((sum, item) => sum += item.length, 0);
-
-const sizeArr = getSizeArray(countChars);
-
-let isOk = false;
-
-
-// Игровое поле
-let gameArr = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x).fill(0) );
-
-// массив для наглядности со свободными соседними ячейками - удалить
-let neighborsArr = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x) );
-
-// массив объектов со свободными соседними ячейками и координатами
-let neighborsArrObj = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x) );
-
-for (let y = 0; y < sizeArr.y; y++) {
-
-    for (let x = 0; x < sizeArr.x; x++) {
-        let freeNeighbors = 0;
-
-        if ( (x === 0 && y === 0) || (x === sizeArr.x - 1 && y === 0) || (x === 0 && y === sizeArr.y - 1) || (x === sizeArr.x - 1 && y === sizeArr.y - 1) ) {
-            freeNeighbors = 2;
-        } else if ( (x > 0 && x < sizeArr.x - 1 && y === 0) || (x === 0 && y > 0 && y < sizeArr.y - 1) || (x === sizeArr.x - 1 && y > 0 && y < sizeArr.y - 1) || (x > 0 && x < sizeArr.x - 1 && y === sizeArr.y - 1) ) {
-            freeNeighbors = 3;
-        } else {
-            freeNeighbors = 4;
-        }
-
-        const obj = {
-            x: x,
-            y: y,
-            char: '',
-            color: '',
-            isEmpty: true,
-            freeNeighbors: freeNeighbors
-        };
-
-        neighborsArrObj[y][x] = obj;
-        neighborsArr[y][x] = obj.freeNeighbors;
+$hideColor.addEventListener('change', function(e) {
+    console.log(this.checked);
+    const $field = document.getElementById('game');
+    if (this.checked) {
+        $field.classList.add('hide-colors');
+    } else {
+        $field.classList.remove('hide-colors');
     }
-}
+})
 
-// console.log(countChars, sizeArr);
-console.log(gameArr);
-console.time('CreateField');
-countAttemps = 1;
+$btn.addEventListener('click', function(e) {
+    const $textarea = document.getElementById('words');
 
-while (!isOk) {
+    words = $textarea.value.replace(/[0-9-_.;: ]*/gi, '').split(',');
+    console.log(words);
 
-    try {
-        words.forEach((word, index) => {
+    console.time('CreateField');
+    const timeStart = new Date().getTime();
+    let gameField = getGameField(words, colors);
+    const timeEnd = new Date().getTime();
+    console.timeEnd('CreateField');
+    const timeCreate = timeEnd - timeStart;
 
-            let startX, startY;
+    console.log('Кол-во попыток построения: ', gameField.countAttemps);
+    console.log('Время создания: ', timeCreate, 'ms');
 
-            if (index === 0) {
-                startX = getRandomInt(0, sizeArr.x - 1); 
-                startY = getRandomInt(0, sizeArr.y - 1);
-            } else {
-                let min = 2;
+    const $infoList = document.getElementById('info_list');
+    const $infoTime = document.getElementById('time');
+    const $infoCount = document.getElementById('count');
+    $infoList.classList.remove('hide');
 
-                let minPoints = [];
+    $infoTime.innerText = timeCreate + 'ms';
+    $infoCount.innerText = gameField.countAttemps;
 
-                for (let y = 0; y < sizeArr.y; y++) {
-                    for (let x = 0; x < sizeArr.x; x++) {
-                        if ( neighborsArrObj[y][x].isEmpty && neighborsArrObj[y][x].freeNeighbors <= min) {
-                            min = neighborsArrObj[y][x].freeNeighbors;
-                            minPoints.push(neighborsArrObj[y][x]);
-                        }
-                    }
-                }
+    clearField();
+    drawField(gameField.field);
+});
 
-                let minPoint = shuffle(minPoints)[0];
 
-                startX = minPoint.x; 
-                startY = minPoint.y;
-            }
 
-            let wordObj = {
-                chars: Array.from(word).reverse() ,
-                // color: colors.pop(),
-                x: startX,
-                y: startY,
-                direction: ''
-            };
-
-            // console.log(wordObj);
-
-            let i = 1;
-            while (wordObj.chars.length > 0) {
-                const char = wordObj.chars.pop();
-                // console.log(wordObj.chars.length);
-
-                if (wordObj.direction !== '') {
-                    switch(wordObj.direction) {
-                        case 'top':
-                            wordObj.y = wordObj.y - 1;
-                            break;
-                        case 'right':
-                            wordObj.x = wordObj.x + 1;
-                            break;
-                        case 'bottom':
-                            wordObj.y = wordObj.y + 1;
-                            break;
-                        case 'left':
-                            wordObj.x = wordObj.x - 1;
-                    }
-                }
-                gameArr[wordObj.y][wordObj.x] = char;
-
-                
-
-                // console.log('----------');
-                // console.log('word - ', index, ';  stage - ', i++);
-                // console.log(wordObj, char);
-
-                if (wordObj.chars.length !== 0) 
-                    wordObj.direction = findDirection(wordObj.x, wordObj.y, gameArr);
-
-                neighborsArrObj[wordObj.y][wordObj.x].isEmpty = false;
-                neighborsArrObj[wordObj.y][wordObj.x].char = char;
-                neighborsArrObj[wordObj.y][wordObj.x].color = colors[colorIndex];
-
-                if ( wordObj.y - 1 >= 0 ) {
-                    --neighborsArrObj[wordObj.y - 1][wordObj.x].freeNeighbors;
-
-                    if (!neighborsArrObj[wordObj.y - 1][wordObj.x].isEmpty) {
-                        neighborsArr[wordObj.y - 1][wordObj.x] = 0;
-                    } else {
-                        neighborsArr[wordObj.y - 1][wordObj.x] = neighborsArrObj[wordObj.y - 1][wordObj.x].freeNeighbors;
-                    }
-                    
-                }
-                if ( wordObj.y + 1 < sizeArr.y ) {
-                    --neighborsArrObj[wordObj.y + 1][wordObj.x].freeNeighbors;
-
-                    if (!neighborsArrObj[wordObj.y + 1][wordObj.x].isEmpty) {
-                        neighborsArr[wordObj.y + 1][wordObj.x] = 0;
-                    } else {
-                        neighborsArr[wordObj.y + 1][wordObj.x] = neighborsArrObj[wordObj.y + 1][wordObj.x].freeNeighbors;
-                    }
-
-                }
-                if ( wordObj.x - 1 >= 0 ) {
-                    --neighborsArrObj[wordObj.y][wordObj.x - 1].freeNeighbors;
-
-                    if (!neighborsArrObj[wordObj.y][wordObj.x - 1].isEmpty) {
-                        neighborsArr[wordObj.y][wordObj.x - 1] = 0;
-                    } else {
-                        neighborsArr[wordObj.y][wordObj.x - 1] = neighborsArrObj[wordObj.y][wordObj.x - 1].freeNeighbors;
-                    }
-                    
-                }
-                if ( wordObj.x + 1 < sizeArr.x ) {
-                    --neighborsArrObj[wordObj.y][wordObj.x + 1].freeNeighbors;
-
-                    if (!neighborsArrObj[wordObj.y][wordObj.x + 1].isEmpty) {
-                        neighborsArr[wordObj.y][wordObj.x + 1] = 0;
-                    } else {
-                        neighborsArr[wordObj.y][wordObj.x + 1] = neighborsArrObj[wordObj.y][wordObj.x + 1].freeNeighbors;
-                    }
-                }
-
-                if (!neighborsArrObj[wordObj.y][wordObj.x].isEmpty) {
-                    neighborsArr[wordObj.y][wordObj.x] = 0;
-                }
-                
-                // console.log(gameArr);
-            }
-
-            colorIndex = colorIndex < colors.length - 1 ? colorIndex + 1 : 0;
-
-        });
-
-        isOk = true;
-    } catch {
-        countAttemps++;
-        console.log('Попытка - ', countAttemps);
-        gameArr = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x).fill(0) );
-
-        // массив для наглядности со свободными соседними ячейками - удалить
-        neighborsArr = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x) );
-
-        // массив объектов со свободными соседними ячейками и координатами
-        neighborsArrObj = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x) );
-
-        for (let y = 0; y < sizeArr.y; y++) {
-
-            for (let x = 0; x < sizeArr.x; x++) {
-                let freeNeighbors = 0;
-        
-                if ( (x === 0 && y === 0) || (x === sizeArr.x - 1 && y === 0) || (x === 0 && y === sizeArr.y - 1) || (x === sizeArr.x - 1 && y === sizeArr.y - 1) ) {
-                    freeNeighbors = 2;
-                } else if ( (x > 0 && x < sizeArr.x - 1 && y === 0) || (x === 0 && y > 0 && y < sizeArr.y - 1) || (x === sizeArr.x - 1 && y > 0 && y < sizeArr.y - 1) || (x > 0 && x < sizeArr.x - 1 && y === sizeArr.y - 1) ) {
-                    freeNeighbors = 3;
-                } else {
-                    freeNeighbors = 4;
-                }
-        
-                const obj = {
-                    x: x,
-                    y: y,
-                    char: '',
-                    color: '',
-                    isEmpty: true,
-                    freeNeighbors: freeNeighbors
-                };
-        
-                neighborsArrObj[y][x] = obj;
-                neighborsArr[y][x] = obj.freeNeighbors;
-            }
-        }
-    }
-}
-
-// console.log(gameArr);
-// console.log(neighborsArr);
-//     console.log(neighborsArrObj);
-// drawField(gameArr);
-console.timeEnd('CreateField');
-
-console.log('Кол-во попыток построения: ', countAttemps);
-drawField(neighborsArrObj);
 
 
 function getSizeArray(countItems) {
@@ -392,6 +191,13 @@ function drawField(gameArr) {
     }
 }
 
+function clearField() {
+    console.log('clear');
+    const $field = document.getElementById('game');
+
+    $field.innerHTML = '';
+}
+
 
 // classNames - строка классов, классы разделены пробелами
 function createElement(tag, classNames = '', innerText = '') {
@@ -404,4 +210,218 @@ function createElement(tag, classNames = '', innerText = '') {
         $element.innerText = innerText;
 
     return $element;
+}
+
+function getGameField(words, colors) {
+
+    let = colorIndex = 0;
+    colors = shuffle(colors);
+
+    const countChars = words.reduce((sum, item) => sum += item.length, 0);
+
+    const sizeArr = getSizeArray(countChars);
+
+    let isGameFieldCreated = false;
+
+    // Игровое поле
+    let gameArr = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x).fill(0) );
+
+    // массив для наглядности со свободными соседними ячейками - удалить
+    let neighborsArr = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x) );
+
+    // массив объектов со свободными соседними ячейками и координатами
+    let neighborsArrObj = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x) );
+
+    for (let y = 0; y < sizeArr.y; y++) {
+
+        for (let x = 0; x < sizeArr.x; x++) {
+            let freeNeighbors = 0;
+
+            if ( (x === 0 && y === 0) || (x === sizeArr.x - 1 && y === 0) || (x === 0 && y === sizeArr.y - 1) || (x === sizeArr.x - 1 && y === sizeArr.y - 1) ) {
+                freeNeighbors = 2;
+            } else if ( (x > 0 && x < sizeArr.x - 1 && y === 0) || (x === 0 && y > 0 && y < sizeArr.y - 1) || (x === sizeArr.x - 1 && y > 0 && y < sizeArr.y - 1) || (x > 0 && x < sizeArr.x - 1 && y === sizeArr.y - 1) ) {
+                freeNeighbors = 3;
+            } else {
+                freeNeighbors = 4;
+            }
+
+            const obj = {
+                x: x,
+                y: y,
+                char: '',
+                color: '',
+                isEmpty: true,
+                freeNeighbors: freeNeighbors
+            };
+
+            neighborsArrObj[y][x] = obj;
+            neighborsArr[y][x] = obj.freeNeighbors;
+        }
+    }
+
+    // console.log(countChars, sizeArr);
+    console.log(gameArr);
+    let countAttemps = 1;
+
+    while (!isGameFieldCreated) {
+
+        try {
+            words.forEach((word, index) => {
+
+                let startX, startY;
+
+                if (index === 0) {
+                    startX = getRandomInt(0, sizeArr.x - 1); 
+                    startY = getRandomInt(0, sizeArr.y - 1);
+                } else {
+                    let min = 2;
+
+                    let minPoints = [];
+
+                    for (let y = 0; y < sizeArr.y; y++) {
+                        for (let x = 0; x < sizeArr.x; x++) {
+                            if ( neighborsArrObj[y][x].isEmpty && neighborsArrObj[y][x].freeNeighbors <= min) {
+                                min = neighborsArrObj[y][x].freeNeighbors;
+                                minPoints.push(neighborsArrObj[y][x]);
+                            }
+                        }
+                    }
+
+                    let minPoint = shuffle(minPoints)[0];
+
+                    startX = minPoint.x; 
+                    startY = minPoint.y;
+                }
+
+                let wordObj = {
+                    chars: Array.from(word).reverse() ,
+                    x: startX,
+                    y: startY,
+                    direction: ''
+                };
+
+                let i = 1;
+                while (wordObj.chars.length > 0) {
+                    const char = wordObj.chars.pop();
+                    // console.log(wordObj.chars.length);
+
+                    if (wordObj.direction !== '') {
+                        switch(wordObj.direction) {
+                            case 'top':
+                                wordObj.y = wordObj.y - 1;
+                                break;
+                            case 'right':
+                                wordObj.x = wordObj.x + 1;
+                                break;
+                            case 'bottom':
+                                wordObj.y = wordObj.y + 1;
+                                break;
+                            case 'left':
+                                wordObj.x = wordObj.x - 1;
+                        }
+                    }
+                    gameArr[wordObj.y][wordObj.x] = char;
+
+                    if (wordObj.chars.length !== 0) 
+                        wordObj.direction = findDirection(wordObj.x, wordObj.y, gameArr);
+
+                    neighborsArrObj[wordObj.y][wordObj.x].isEmpty = false;
+                    neighborsArrObj[wordObj.y][wordObj.x].char = char;
+                    neighborsArrObj[wordObj.y][wordObj.x].color = colors[colorIndex];
+
+                    if ( wordObj.y - 1 >= 0 ) {
+                        --neighborsArrObj[wordObj.y - 1][wordObj.x].freeNeighbors;
+
+                        if (!neighborsArrObj[wordObj.y - 1][wordObj.x].isEmpty) {
+                            neighborsArr[wordObj.y - 1][wordObj.x] = 0;
+                        } else {
+                            neighborsArr[wordObj.y - 1][wordObj.x] = neighborsArrObj[wordObj.y - 1][wordObj.x].freeNeighbors;
+                        }
+                        
+                    }
+                    if ( wordObj.y + 1 < sizeArr.y ) {
+                        --neighborsArrObj[wordObj.y + 1][wordObj.x].freeNeighbors;
+
+                        if (!neighborsArrObj[wordObj.y + 1][wordObj.x].isEmpty) {
+                            neighborsArr[wordObj.y + 1][wordObj.x] = 0;
+                        } else {
+                            neighborsArr[wordObj.y + 1][wordObj.x] = neighborsArrObj[wordObj.y + 1][wordObj.x].freeNeighbors;
+                        }
+
+                    }
+                    if ( wordObj.x - 1 >= 0 ) {
+                        --neighborsArrObj[wordObj.y][wordObj.x - 1].freeNeighbors;
+
+                        if (!neighborsArrObj[wordObj.y][wordObj.x - 1].isEmpty) {
+                            neighborsArr[wordObj.y][wordObj.x - 1] = 0;
+                        } else {
+                            neighborsArr[wordObj.y][wordObj.x - 1] = neighborsArrObj[wordObj.y][wordObj.x - 1].freeNeighbors;
+                        }
+                        
+                    }
+                    if ( wordObj.x + 1 < sizeArr.x ) {
+                        --neighborsArrObj[wordObj.y][wordObj.x + 1].freeNeighbors;
+
+                        if (!neighborsArrObj[wordObj.y][wordObj.x + 1].isEmpty) {
+                            neighborsArr[wordObj.y][wordObj.x + 1] = 0;
+                        } else {
+                            neighborsArr[wordObj.y][wordObj.x + 1] = neighborsArrObj[wordObj.y][wordObj.x + 1].freeNeighbors;
+                        }
+                    }
+
+                    if (!neighborsArrObj[wordObj.y][wordObj.x].isEmpty) {
+                        neighborsArr[wordObj.y][wordObj.x] = 0;
+                    }
+                    
+                }
+
+                colorIndex = colorIndex < colors.length - 1 ? colorIndex + 1 : 0;
+
+            });
+
+            isGameFieldCreated = true;
+        } catch {
+            countAttemps++;
+            console.log('Попытка - ', countAttemps);
+            gameArr = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x).fill(0) );
+
+            // массив для наглядности со свободными соседними ячейками - удалить
+            neighborsArr = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x) );
+
+            // массив объектов со свободными соседними ячейками и координатами
+            neighborsArrObj = Array.from( Array(sizeArr.y), () => new Array(sizeArr.x) );
+
+            for (let y = 0; y < sizeArr.y; y++) {
+
+                for (let x = 0; x < sizeArr.x; x++) {
+                    let freeNeighbors = 0;
+            
+                    if ( (x === 0 && y === 0) || (x === sizeArr.x - 1 && y === 0) || (x === 0 && y === sizeArr.y - 1) || (x === sizeArr.x - 1 && y === sizeArr.y - 1) ) {
+                        freeNeighbors = 2;
+                    } else if ( (x > 0 && x < sizeArr.x - 1 && y === 0) || (x === 0 && y > 0 && y < sizeArr.y - 1) || (x === sizeArr.x - 1 && y > 0 && y < sizeArr.y - 1) || (x > 0 && x < sizeArr.x - 1 && y === sizeArr.y - 1) ) {
+                        freeNeighbors = 3;
+                    } else {
+                        freeNeighbors = 4;
+                    }
+            
+                    const obj = {
+                        x: x,
+                        y: y,
+                        char: '',
+                        color: '',
+                        isEmpty: true,
+                        freeNeighbors: freeNeighbors
+                    };
+            
+                    neighborsArrObj[y][x] = obj;
+                    neighborsArr[y][x] = obj.freeNeighbors;
+                }
+            }
+        }
+    }
+
+    return {
+        field: neighborsArrObj,
+        countAttemps: countAttemps
+    };
 }
